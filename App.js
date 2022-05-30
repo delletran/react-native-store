8ab0444e9aca7c1c1e0ce0b63052badb3561c435
rodelletran <rodel.letran@sun-asterisk.com>
2022-05-30 18:54:10 +0800

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yo, Fuckers! </Text>
+      <Text>Yo, Fellas</Text>
       <StatusBar style="auto" />
     </View>
   );
